beec72b12f7fd419b4719e6c52b3a8a21a5556ae
teamolhuang <82561200+teamolhuang@users.noreply.github.com>
2023-02-17 10:23:03 +0800

    Add: 設定好 EF 並初步 Scaffold

---
+++ Models/Entities/ForumContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoForum.Models.Entities;
+
+public partial class ForumContext : DbContext
+{
+    public ForumContext()
+    {
+    }
+
+    public ForumContext(DbContextOptions<ForumContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Post> Posts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:DemoForum");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("forum");
+
+        modelBuilder.Entity<Post>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("Post_pk");
+
+            entity.ToTable("Post");
+
+            entity.Property(e => e.Content).HasMaxLength(2000);
+            entity.Property(e => e.Title).HasMaxLength(20);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Entities/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoForum.Models.Entities;
+
+public partial class Post
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Content { get; set; } = null!;
+}
