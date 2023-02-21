95240334fa90d3bc0463924147bacf95c5716c96
teamolhuang <82561200+teamolhuang@users.noreply.github.com>
2023-02-21 15:05:10 +0800

    Add: 增加 StaticFunctions 單元測試

---
+++ UnitTests/Utils/StaticFunctionsTests.cs
@@ -0,0 +1,60 @@
+﻿using DemoForum.Utils;
+using NUnit.Framework;
+using System;
+
+namespace DemoForumTests.Utils;
+
+public class StaticFunctionsTests
+{
+    private const string DefaultSuffix = " ...";
+
+    [Test]
+    [TestCase("字", 20, DefaultSuffix, "字")]
+    [TestCase("字字字", 2, DefaultSuffix, "字字" + DefaultSuffix)]
+    [TestCase("我我aaa", 4, "b", "我我aab")]
+    [TestCase("我我aaa", 5, "b", "我我aaa")]
+    public void ShortenToPreview_ShouldTurnLongStringIntoPreviewText_AndReturn(string s, int len, string suffix, string expected)
+    {
+        // Act
+        string actual = s.ShortenToPreview(len, suffix);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+    
+    [Test]
+    [TestCase("abcddd")]
+    [TestCase("abcdefghijklmnopqrstuvwxyz")]
+    [TestCase("一二三")]
+    [TestCase("一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十")]
+    public void ShortenToDefault_ShouldTurnLongStringIntoPreviewTextByDefaultFormat_AndReturn(string s)
+    {
+        // Arrange
+        string expected = Arrange_FormatByDefaultLengthAndSuffix(s);
+        
+        // Act
+        string actual = s.ShortenToPreviewDefault();
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+    
+    [Test]
+    [TestCase("字", 20, "字")]
+    [TestCase("字字字", 2, "字字" + DefaultSuffix)]
+    [TestCase("我我aaa", 4, "我我aa" + DefaultSuffix)]
+    [TestCase("我我aaa", 5, "我我aaa")]
+    public void ShortenToDefault_ShouldTurnLongStringIntoPreviewTextByDefaultFormat_AndReturn(string s, int len, string expected)
+    {
+        // Act
+        string actual = s.ShortenToPreviewDefault(len);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    private static string Arrange_FormatByDefaultLengthAndSuffix(string actual)
+    {
+        return actual.Length > 20 ? string.Concat(actual.AsSpan(0, 20), DefaultSuffix) : actual;
+    }
+}
