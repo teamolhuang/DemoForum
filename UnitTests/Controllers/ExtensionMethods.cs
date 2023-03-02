81ad8bee4e771efd2cae0e67b0043664a977eaf5
teamolhuang <82561200+teamolhuang@users.noreply.github.com>
2023-03-02 12:20:09 +0800

    Refactor: 補上註解

--- UnitTests/Controllers/ExtensionMethods.cs
+++ UnitTests/Controllers/ExtensionMethods.cs
@@ -4,6 +4,10 @@ namespace DemoForumTests.Controllers;
 
 public static class ExtensionMethods
 {
+    /// <summary>
+    /// Adds a mock error to ModelStateDictionary for ModelState tests.
+    /// </summary>
+    /// <param name="dict">ModelStateDictionary</param>
     public static void AddModelMockError(this ModelStateDictionary dict)
     {
         dict.AddModelError("test", "test");
