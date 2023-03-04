c98ec47ac77f070355f3240f72648539a979551d
teamolhuang <82561200+teamolhuang@users.noreply.github.com>
2023-03-04 13:33:27 +0800

    Add: 防止多重 Postback 的 js

--- Server/wwwroot/js/site.js
+++ Server/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(function () {
+    $('form').submit(function () {
+        $(':submit', this).prop('disabled', true);
+    });
+
+    $('form :input').on('invalid', function () {
+        $(':submit', $(this).parents('form')).prop('disabled', false);
+    });
+});
