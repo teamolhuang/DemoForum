f321ed120d4bd137d3c6bb96ec1687fefef7b860
teamolhuang <82561200+teamolhuang@users.noreply.github.com>
2023-03-04 13:17:51 +0800

    Refactor: 調整 IsLogon 的檢查位置

--- Server/Models/CommentInputViewModel.cs
+++ Server/Models/CommentInputViewModel.cs
@@ -6,6 +6,7 @@ public class CommentInputViewModel
     public string? BooChinese { get; set; }
     public string? NaturalChinese { get; set; }
     public bool? IsUserAuthor { get; set; }
+    public bool? IsLogon { get; set; }
     public int? PostId { get; set; }
     
     public string? CommentContent { get; set; }
