9c5699291b91d30d92ddf5fe26cd30a91cec1624
teamolhuang <82561200+teamolhuang@users.noreply.github.com>
2023-03-06 11:52:40 +0800

    Add: 讓推文和文章內容的 URL 會自動變成超連結

---
+++ Server/Utils/LinkifyHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DemoForum.Utils;
+
+public static class LinkifyHelper
+{
+    const string UrlPattern = "(?:https?://|www\\.)\\S+[/\\b]?";
+
+    /// <summary>
+    /// Linkify urls for views.
+    /// </summary>
+    /// <param name="text">Full string</param>
+    /// <returns>string but with a-hrefs</returns>
+    public static string Linkify(string text)
+    {
+        // Replace URLs with HTML hyperlink elements
+        return Regex.Replace(text, UrlPattern, m =>
+        {
+            string url = m.Value.StartsWith("http") ? m.Value : "http://" + m.Value;
+            return $"<a href='{url}' target='_blank'>{m.Value}</a>"; // target=_blank means open up a new tab 
+        });
+    }
+}
---
+++ UnitTests/Utils/LinkifyHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using DemoForum.Utils;
+using NUnit.Framework;
+
+namespace DemoForumTests.Utils;
+
+public class LinkifyHelperTests
+{
+    [Test]
+    [TestCase("http://www.google.com.tw/", "<a href='http://www.google.com.tw/' target='_blank'>http://www.google.com.tw/</a>")]
+    [TestCase("http://www.google.com.tw", "<a href='http://www.google.com.tw' target='_blank'>http://www.google.com.tw</a>")]
+    [TestCase("https://www.google.com.tw/abc/", "<a href='https://www.google.com.tw/abc/' target='_blank'>https://www.google.com.tw/abc/</a>")]
+    [TestCase("https://www.google.com.tw/abc-def?g=1", "<a href='https://www.google.com.tw/abc-def?g=1' target='_blank'>https://www.google.com.tw/abc-def?g=1</a>")]
+    [TestCase("前面有中文http://www.yahoo.com.tw/和連結連在一起", "前面有中文<a href='http://www.yahoo.com.tw/和連結連在一起' target='_blank'>http://www.yahoo.com.tw/和連結連在一起</a>")]
+    [TestCase("前面有中文http://www.aaa.com.tw/ 和連結分開", "前面有中文<a href='http://www.aaa.com.tw/' target='_blank'>http://www.aaa.com.tw/</a> 和連結分開")]
+    public void Linkify_WillDetectUrlsInText_AndReturnStringWithHtmlTags(string text, string expected)
+    {
+        // Arrange
+        
+        
+        // Act
+        string actual = LinkifyHelper.Linkify(text);
+        
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+}
