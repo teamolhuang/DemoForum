d51fa7730e0b0a7779511a4332986c5fb5d8d4fc
teamolhuang <82561200+teamolhuang@users.noreply.github.com>
2023-03-08 06:50:16 +0800

    Add: HTTPS redirection

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using AspNetCoreHero.ToastNotification;
 using AspNetCoreHero.ToastNotification.Extensions;
 using DemoForum.Models.Entities;
@@ -63,6 +64,16 @@ if (!builder.Environment.IsDevelopment())
         .PersistKeysToFileSystem(new DirectoryInfo("/key/storage"));
 }
 
+// HTTPS
+if (!builder.Environment.IsDevelopment())
+{
+    builder.Services.AddHttpsRedirection(options =>
+    {
+        options.RedirectStatusCode = (int)HttpStatusCode.PermanentRedirect;
+        options.HttpsPort = 443;
+    });
+}
+
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -72,6 +83,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
